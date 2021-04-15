2c8661dcf189abb21c7b94ce980a83c0bbd35c8e
hendratanizar <82543611+hendratanizar@users.noreply.github.com>
2021-04-15 23:29:22 +0700

    Update EchoClient.java

--- EchoClient.java
+++ EchoClient.java
@@ -7,7 +7,7 @@ package EchoServer;
 
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
+import java.util.Scanner; //import scanner
 /**
  *
  * @author USER
@@ -19,14 +19,14 @@ public class EchoClient {
      */
     public static void main(String[] args) {
         try{
-        InetAddress localHost = InetAddress.getLocalHost();
-        Socket socket = new Socket(localHost,8000);
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        InetAddress localHost = InetAddress.getLocalHost(); //untuk mengambil alamat IP
+        Socket socket = new Socket(localHost,8000); //membuat socket baru
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true); //print output
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream())); //untuk membaca sebuah karakter untuk dijadikan program
         
         System.out.println("Telah terkoneksi dengan server");
-        Scanner scanner = new Scanner(System.in);
-        while(true){
+        Scanner scanner = new Scanner(System.in); //untuk mendeteksi inputan
+        while(true){ //apabila sudah terkoneksi
             System.out.println("Tulis kalimat :");
             String input = scanner.nextLine();
             if("exit".equalsIgnoreCase(input)) {
